e8ad219e4843c6c623e17b9cac7feca924735ec2
SadikiMidagu <sadikimidagudeveloper@gmail.com>
2021-10-16 20:57:29 -0500

    fourth commit

--- script.js
+++ script.js
@@ -6,11 +6,12 @@ function calculate()
     r = document.getElementById("r").value;
     if (p<=0) {
         alert("The principal has to be greater than 0");
+        return -1;
     }
 
     result = document.getElementById("result");
     interest = (p*n*r/100);
     currentYear = new Date().getFullYear();
     targetYear = parseInt(n) + currentYear;
-    result.innerHTML = "If you deposit $" + p + "<br> at an interest rate of " + r + "%.<br> You will receive an amount of $" + interest + "<br> in the year " + targetYear + "<br>";
+    result.innerHTML = "If you deposit <mark>$" + p + "<br></mark> at an interest rate of <mark>" + r + "%</mark>.<br> You will receive an amount of <mark>$" + interest + "</mark><br> in the year <mark>" + targetYear + "</mark><br>";
 }
