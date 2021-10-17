3ddbea20e6cf5b158c4e1306373cac55090c8062
SadikiMidagu <sadikimidagudeveloper@gmail.com>
2021-10-16 20:00:38 -0500

    first commit

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-function compute()
+
+function calculate()
 {
-    p = document.getElementById("principal").value;
+    p = document.getElementById("p").value;
+    n = document.getElementById("n").value;
+    r = document.getElementById("r").value;
+    result = document.getElementById("result");
+    interest = (p*n*r/100);
+    currentYear = new Date().getFullYear();
+    targetYear = parseInt(n) + currentYear;
+    
+    result.innerHTML = "If you deposit $" + p + "<br> at an interest rate of " + r + "%.<br> You will receive an amount of $" + interest + "<br> in the year " + targetYear + "<br>";
     
 }
-        
